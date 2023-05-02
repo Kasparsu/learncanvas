0d59ed226abbdd342029e9be77c01eb3f8d5602b
Jüri Kajava <jyri.kajava@tptlive.ee>
2023-05-02 12:58:26 +0300

    enemy now faces the player on the x axis

--- src/Enemy.js
+++ src/Enemy.js
@@ -9,6 +9,11 @@ export class Enemy extends Physics.Arcade.Sprite{
         this.body.setOffset(0, 4);
         this.body.setSize(16, 16, false);
 
+        scene.physics.add.collider(this, player, () => {
+
+            
+        })
+
         this.anims.create({
             key: 'masked_orc_run_anim',
             frames: this.anims.generateFrameNames('atlas', { prefix: 'masked_orc_run_anim_', start:0, end: 3}),
@@ -25,5 +30,11 @@ export class Enemy extends Physics.Arcade.Sprite{
     preUpdate(time, delta){
         super.preUpdate(time, delta);
         this.scene.physics.moveToObject(this, this.player, 100);
+        //right side is -90 degrees to 90 degrees, so I derived it from this equation 1.571rad × 180/π = 90° => 1.571rad = 90° / 180/π
+        if(this.body.angle > -90/(180/Math.PI) && this.body.angle < 90/(180/Math.PI)){
+            this.setFlipX(false);
+        } else {
+            this.setFlipX(true);
+        }
     }
 }
