6576cd8f5c630ddaefcb8a14563c58995bac29a7
Aleksander Dalton <Aleksander.Dalton@tptlive.ee>
2023-05-27 12:01:41 +0300

    Goblin follows player

---
+++ src/Goblin.js
@@ -0,0 +1,41 @@
+import { Physics } from "phaser";
+
+export class Goblin extends Physics.Arcade.Sprite {
+    constructor(scene, x, y, player) {
+        super(scene, x, y, 'atlas', 'goblin_idle_anim_0');
+        scene.physics.add.existing(this);
+        this.body.setOffset(3, 5);
+        this.body.setSize(12, 12, false);
+        this.body.setMaxSpeed(400);
+        this.body.setDrag(800, 800);
+        this.player = player;
+
+        this.anims.create({
+            key: 'goblin_idle_anim',
+            frames: this.anims.generateFrameNames('atlas', { prefix: 'goblin_idle_anim_', start: 0, end: 3 }),
+            repeat: -1,
+            frameRate: 8,
+        });
+
+        this.anims.create({
+            key: 'goblin_run_anim',
+            frames: this.anims.generateFrameNames('atlas', { prefix: 'goblin_run_anim_', start: 0, end: 3 }),
+            repeat: -1,
+            frameRate: 8,
+        });
+
+        this.anims.play('goblin_idle_anim');
+        this.setScale(6);
+    }
+
+    preUpdate(time, delta) {
+        super.preUpdate(time, delta);
+        this.scene.physics.moveToObject(this, this.player, 100);
+
+        if (this.body.speed > 0) {
+            this.anims.play('goblin_run_anim', true);
+        } else {
+            this.anims.play('goblin_idle_anim', true);
+        }
+    }
+}
--- src/MainScene.js
+++ src/MainScene.js
@@ -3,6 +3,7 @@ import atlas from './assets/0x72_DungeonTilesetII_v1.4.png';
 import atlasJSON from './assets/atlas.json';
 import mapJSON from './assets/map.json';
 import { Player } from "./Player";
+import { Goblin } from "./Goblin";
 export class MainScene extends Scene {
 
     map;
@@ -20,10 +21,15 @@ export class MainScene extends Scene {
         floor.setCollisionByExclusion([130]);
         const walls = map.createLayer(1, tiles, 0, 0);
         walls.setScale(4);
-        let player = this.add.existing(new Player(this, 100, 100));
+        let player = this.add.existing(new Player(this, 500, 200));
+
+        let goblin = this.add.existing(new Goblin(this, 500, 500, player));
+
         const edges = map.createLayer(2, tiles, 0, 0);
         edges.setScale(4);
         this.physics.add.collider(player, floor);
+
+        this.physics.add.collider(goblin, floor);
         
         this.map = map;
     }
