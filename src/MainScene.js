b07315e996167a55a3fbf1a74491634ecfbd7c5b
gloriavimb <gloria.vimberg@tptlive.ee>
2023-05-28 19:07:26 +0300

    Made so arrows collide with walls

--- src/MainScene.js
+++ src/MainScene.js
@@ -4,6 +4,7 @@ import atlasJSON from './assets/atlas.json';
 import mapJSON from './assets/map.json';
 import { Player } from "./Player";
 export class MainScene extends Scene {
+    map;
       
     preload() {
         this.load.atlas('atlas', atlas, atlasJSON);
@@ -18,11 +19,13 @@ export class MainScene extends Scene {
         floor.setCollisionByExclusion([130]);
         const walls = map.createLayer(1, tiles, 0, 0);
         walls.setScale(4);
+        walls.setCollisionByExclusion([130]);
         let player = this.add.existing(new Player(this, 100, 100));
         const edges = map.createLayer(2, tiles, 0, 0);
         edges.setScale(4);
         this.physics.add.collider(player, floor);
         
+        this.map = map;
     }
     
 }
