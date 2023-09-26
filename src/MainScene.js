e5c4a771ff57b3b30a29fe7dff7ee5368e9dc9ce
AhmadMuhhamedsin <73689912+AhmadMuhhamedsin@users.noreply.github.com>
2023-09-26 19:12:31 +0300

    Add an enemy (goblin) class
    Added an enemy class

--- src/MainScene.js
+++ src/MainScene.js
@@ -3,8 +3,9 @@ import atlas from './assets/0x72_DungeonTilesetII_v1.4.png';
 import atlasJSON from './assets/atlas.json';
 import mapJSON from './assets/map.json';
 import { Player } from "./Player";
+import { Enemy } from "./Goblin";
 export class MainScene extends Scene {
-      
+    map;
     preload() {
         this.load.atlas('atlas', atlas, atlasJSON);
         this.load.tilemapTiledJSON('map', mapJSON);
@@ -13,16 +14,26 @@ export class MainScene extends Scene {
     create() {
         const map = this.make.tilemap({ key: 'map' });
         const tiles = map.addTilesetImage('0x72_DungeonTilesetII_v1.4', 'atlas');
+
         const floor = map.createLayer(0, tiles, 0, 0);
         floor.setScale(4);
         floor.setCollisionByExclusion([130]);
+
         const walls = map.createLayer(1, tiles, 0, 0);
         walls.setScale(4);
+
         let player = this.add.existing(new Player(this, 100, 100));
+
+        let goblin = this.add.existing(new Enemy(this, 800, 400, player))
+        this.goblin = goblin
+
         const edges = map.createLayer(2, tiles, 0, 0);
         edges.setScale(4);
+
         this.physics.add.collider(player, floor);
-        
+        this.physics.add.collider(goblin, floor);
+
+        this.map = map;
     }
     
 }
