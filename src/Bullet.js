a001480a242162cce319712c90f456a59dccc00e
sk0ut <andreas.hyys@gmail.com>
2023-11-04 18:26:46 +0200

    finished tasks

--- src/Bullet.js
+++ src/Bullet.js
@@ -1,20 +1,55 @@
 import * as Phaser from "phaser";
 
 export class Bullet extends Phaser.Physics.Arcade.Sprite {
+    damage = 1;
 
-    constructor(scene,x,y, target){
+    constructor(scene, x, y, target){
         super(scene, x, y);
         scene.physics.add.existing(this);
-        this.setTexture('atlas', 'weapon_arrow');
+        this.setTexture('atlas', 'weapon_axe');
         this.setScale(4);
         this.body.setMaxSpeed(800);
         this.body.useDamping = true;
-        //this.body.setDrag(0.5, 0.5);
+        this.body.setSize(14, 14);
+        this.body.setAngularAcceleration(5000);
         console.log(Phaser.Math.Angle.BetweenPoints(this, target));
         this.rotation = Phaser.Math.Angle.BetweenPoints(this, target)+Math.PI/2;
         
- 
         scene.physics.moveToObject(this, target, 800);
+
+        scene.physics.add.overlap(this, scene.goblins, (bullet, goblin)=>{
+            goblin.takeDamage(this.damage);
+            if(goblin.hp <= 0){
+                scene.player.ammo+=5;
+                scene.player.ammoCounter.emit('ammoChanged', scene.player.ammo);
+            }
+            bullet.destroy();
+        });
+
+        scene.physics.add.collider(this, scene.map.getLayer("Floor").tilemapLayer, () => {
+            this.body.setVelocity(0);
+            this.body.setAngularAcceleration(0);
+            this.body.setAngularVelocity(0);
+
+            scene.physics.add.overlap(this, scene.player, (axe, player) => {
+                axe.destroy();
+                player.ammo++;
+                player.ammoCounter.emit('ammoChanged', player.ammo);
+            });
+
+            // Fade out and destroy object (before ammo pickup task)
+            // scene.tweens.add({
+            //     targets: this,
+            //     alpha: 0,
+            //     duration: 1000,
+            //     ease: 'Linear',
+            //     repeat: 0,
+            //     yoyo: false,
+            //     onComplete: () => {
+            //         this.destroy();
+            //     }
+            // });
+        });
     }
 
     // preUpdate(time,delta){
